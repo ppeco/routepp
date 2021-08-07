39ef4b8d44b4f3d20f320ebd5ee2d4f404d93aae
levkopo <40952805+levkopo@users.noreply.github.com>
2021-08-07 17:32:11 +0500

    Update Router.php

--- src/ppeco/router/Router.php
+++ src/ppeco/router/Router.php
@@ -7,8 +7,8 @@ use JetBrains\PhpStorm\Language;
 class Router {
     public static function route(#[Language("regex")] string $regex,
                                  callable $router) {
-        if(isset($_GET['__route'])&&
-                preg_match("$regex$", $_GET['__route'], $data)){
+        if(isset($_SERVER["REQUEST_URI"])&&
+                preg_match("$regex$", $_SERVER["REQUEST_URI"], $data)){
             echo $router($data);
             exit;
         }
