870f15376523304f5da350ddb793f156b643dc81
Лев Копорушкин <levkopoag@gmail.com>
2021-08-25 13:28:15 +0500

    Update Router.php

--- src/ppeco/router/Router.php
+++ src/ppeco/router/Router.php
@@ -6,14 +6,14 @@ use Closure;
 use JetBrains\PhpStorm\Language;
 
 class Router {
-    public static Closure $output;
+    public static ?Closure $output = null;
 
     public static function route(#[Language("regex")] string $regex,
                                  callable $router) {
         $regex = str_replace("/", "\\/", $regex);
         if(isset($_SERVER["REQUEST_URI"])&&
-                preg_match("/$regex$/", $_SERVER["REQUEST_URI"], $data)){
-            (self::$output??function($output) { echo $output; })->call(self::$output, $router($data));
+            preg_match("/$regex$/", $_SERVER["REQUEST_URI"], $data)){
+            (self::$output??function($output) { echo $output; })->call(new Router(), $router($data));
             exit;
         }
     }
